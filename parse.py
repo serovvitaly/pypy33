c7645ab511d6e7337170e8a69dff36db536b8006
Vitaly Serov <vi.serov@biglion.ru>
2016-05-25 18:45:12 +0300

    пополнена база пользователей, создана база контактов

--- parse.py
+++ parse.py
@@ -11,6 +11,7 @@ class parser:
     country = None
     birth_date = None
     site = None
+    sex = 0
 
     def get_content(self, source_url):
         if source_url is None:
@@ -36,6 +37,49 @@ class borda1(parser):
         self.email = data_mix[5]
         self.site = data_mix[6]
         self.info = data_mix[7]
+        self.sex = 0
+
+
+class ovcharka_kamrbb(parser):
+    def __init__(self, source_url):
+        page_content = self.get_content(source_url)
+        result = re.findall(
+            r"<td( width=21\%|)>([^<]+)<\/td><td>([^<]*)<\/td>",
+            page_content, re.MULTILINE)
+
+        self.nic = None
+        self.full_name = None
+        self.city = None
+        self.country = None
+        self.birth_date = None
+        self.email = None
+        self.site = None
+        self.info = None
+        self.sex = 0
+
+        for row in result:
+            row_name = row[1]
+            row_value = row[2]
+            #print(row[1] + ' = ' + row[2])
+            if row_name == 'Имя':
+                self.nic = row_value
+            if row_name == 'Ф.И.О.':
+                self.full_name = row_value
+            if row_name == 'E-mail':
+                self.email = row_value
+            if row_name == 'Сайт':
+                self.site = row_value
+            if row_name == 'Пол':
+                if row_value == 'женский':
+                    self.sex = 1
+                if row_value == 'мужской':
+                    self.sex = 2
+            if row_name == 'Город':
+                self.city = row_value
+            if row_name == 'Страна':
+                self.country = row_value
+            if row_name == 'День рождения':
+                self.birth_date = row_value
 
 
 conn = sqlite3.connect('dogs.db')
@@ -49,11 +93,15 @@ for row in result:
     owner_id = row[0]
     source_url = row[1]
     parser_class_name = row[2] + '("' + source_url + '")'
-    #print(str(owner_id) + ': ' + source_url)
+    print(str(owner_id) + ': ' + source_url)
     parser_obj = eval(parser_class_name)
     data_obj = parser_obj.__dict__
     data_obj['owner_id'] = owner_id
+    #print(data_obj)
     execute_items.append(data_obj)
 
-c.executemany("UPDATE owners SET nic=:nic, full_name=:full_name, city=:city, country=:country, birth_date=:birth_date, email=:email, site=:site, info=:info WHERE id=:owner_id", execute_items)
+
+print('Запись в базу данных...')
+c.executemany("UPDATE owners SET nic=:nic, full_name=:full_name, city=:city, country=:country, birth_date=:birth_date, email=:email, site=:site, info=:info, sex=:sex WHERE id=:owner_id", execute_items)
 conn.commit()
+conn.close()
--- t2.py
+++ t2.py
@@ -1,8 +1,19 @@
+import sqlite3
+import csv
 
-test_name = 'test1'
+conn = sqlite3.connect('dogs.db')
+c = conn.cursor()
 
-class test1:
-    def __init__(self):
-        print('INIT...')
+base_url = 'http://ovcharka.kamrbb.ru/?x=prof&id='
 
-eval(test_name+'()')
+with open('users2.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+    for row in spamreader:
+        if row[0] == '#':
+            continue
+        source_url = base_url + row[0]
+        print(source_url)
+        c.execute("INSERT INTO owners (source_url, dog_breed, parser_id) VALUES('" + source_url + "', 1, 2)")
+
+conn.commit()
+conn.close()
